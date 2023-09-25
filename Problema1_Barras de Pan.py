e27c3dab13108be376622777de0a06998c809072
reynayr <142543160+reynayr@users.noreply.github.com>
2023-09-25 11:29:32 -0600

    Add files via upload

---
+++ Problema1_Barras de Pan.py
@@ -0,0 +1,17 @@
+﻿'''
+ Autor: Reyna Yazmin Rios Martinez
+ Fecha: 25 de Septiembre de 2023
+ Descripcion: 
+'''
+precio = 3.49
+descuento = 2.094
+
+
+pan = int(input("Barras vendidas, que no son del dia: "))
+
+print ("Precio por barra de pan: $3.49")
+print ("Descuento de 60%, que es igual a $2.094, por no ser una barra fresca")
+
+costoFinal = precio - descuento
+
+print (costoFinal)
